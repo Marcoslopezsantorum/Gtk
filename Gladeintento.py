bbb583febe7d3c7842da4970e28613ee80467337
Mlopezsantorum <mlopezsantorum@danielcastelao.org>
2021-12-01 10:58:08 +0100

    GLADEINTENTOfunciona2

--- Gladeintento.py
+++ Gladeintento.py
@@ -28,6 +28,23 @@ class Aplicacion(Gtk.Window):
         lblApariencia.set_justify((Gtk.Justification.LEFT))
         caixaV.pack_start(lblApariencia,True,True,2)
 
+        rede = Gtk.Grid()
+        caixaV.pack_start(rede,True,True,0)
+        lblEtiqueta = Gtk.Label()
+        lblEtiqueta.set_markup("<i>Etiqueta</i>")
+        rede.add(lblEtiqueta)
+
+        caixa = Gtk.Box (orientation = Gtk.Orientation.HORIZONTAL)
+        txvEtiqueta= Gtk.TextView()
+        txvEtiqueta.set_size_request(300,50)
+        caixa.pack_start(txvEtiqueta,True, True, 0)
+
+        btnEditarEtiqueta = Gtk.Button()
+        imaxe = Gtk.Image.new_from_icon_name('preferences-other', Gtk.IconSize.BUTTON)
+        btnEditarEtiqueta.set_image(imaxe)
+        caixa.pack_start(btnEditarEtiqueta,True,False,0)
+        rede.attach_next_to(caixa,lblEtiqueta,Gtk.PositionType.RIGHT,3,2)
+
 
         self.connect("destroy",Gtk.main_quit)
         self.show_all()
